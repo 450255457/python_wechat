59d36c6da27caed03ea55280d5485dd13d1194e3
450255457 <450255457@qq.com>
2016-04-08 20:30:27 +0800

    04082030
    init

---
+++ wechat.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import tornado.ioloop
+import tornado.web
+import hashlib
+import commands
+import xml.etree.ElementTree as ET
+import time
+
+def check_signature(signature, timestamp, nonce):
+    args = []
+    # 微信公众平台里输入的token
+    args.append('linden')
+    args.append(timestamp)
+    args.append(nonce)
+    args.sort()
+    mysig = hashlib.sha1(''.join(args)).hexdigest()
+    return mysig == signature
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        signature = self.get_argument('signature')
+        timestamp = self.get_argument('timestamp')
+        nonce = self.get_argument('nonce')
+        echostr = self.get_argument('echostr')
+        if check_signature(signature, timestamp, nonce):
+            self.write(echostr)
+        else:
+            self.write('fail')
+    def post(self): 
+        body = self.request.body
+        data = ET.fromstring(body)
+        tousername = data.find('ToUserName').text
+        fromusername = data.find('FromUserName').text
+        createtime = data.find('CreateTime').text
+        msgtype = data.find('MsgType').text
+        content = data.find('Content').text
+        msgid = data.find('MsgId').text
+        if content.strip() in ('ls','pwd','w','uptime'):
+            result = commands.getoutput(content)
+        else:
+            result = 'test'
+        textTpl = """<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[%s]]></MsgType>
+            <Content><![CDATA[%s]]></Content>
+            </xml>"""
+        out = textTpl % (fromusername, tousername, str(int(time.time())), msgtype, result)
+        self.write(out)
+        
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(80)
+    tornado.ioloop.IOLoop.instance().start()
