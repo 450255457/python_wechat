7f533837bb9767254b26681760f8675a9d1b6d96
450255457 <450255457@qq.com>
2016-04-08 20:59:35 +0800

    04082059

--- wechat.py
+++ wechat.py
@@ -4,9 +4,9 @@
 import tornado.ioloop
 import tornado.web
 import hashlib
-import commands
 import xml.etree.ElementTree as ET
 import time
+import os
 
 def check_signature(signature, timestamp, nonce):
     args = []
@@ -35,20 +35,23 @@ class MainHandler(tornado.web.RequestHandler):
         fromUser = data.find('FromUserName').text
         msgType = data.find('MsgType').text
         content = data.find('Content').text
-        textTpl = """<xml>
-            <ToUserName><![CDATA[%s]]></ToUserName>
-            <FromUserName><![CDATA[%s]]></FromUserName>
-            <CreateTime>%s</CreateTime>
-            <MsgType><![CDATA[%s]]></MsgType>
-            <Content><![CDATA[%s]]></Content>
-            </xml>"""
-        out = textTpl % (fromUser, toUser, str(int(time.time())), msgType, content)
-        self.write(out)
+        #textTpl = """<xml>
+        #    <ToUserName><![CDATA[%s]]></ToUserName>
+        #    <FromUserName><![CDATA[%s]]></FromUserName>
+        #    <CreateTime>%s</CreateTime>
+        #    <MsgType><![CDATA[%s]]></MsgType>
+        #    <Content><![CDATA[%s]]></Content>
+        #    </xml>"""
+        # out = textTpl % (fromUser, toUser, str(int(time.time())), msgType, content)
+        # self.write(out)
+        self.render('reply_text.html', toUser=toUser, fromUser=fromUser, content=content)
         
 application = tornado.web.Application([
     (r"/", MainHandler),
+    # (r'/reply_text', MainHandler)],
 ])
 
 if __name__ == "__main__":
+    template_path=os.path.join(os.path.dirname(__file__), "templates")
     application.listen(80)
     tornado.ioloop.IOLoop.instance().start()
