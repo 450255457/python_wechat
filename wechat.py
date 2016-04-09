42520c630e8478c3031cbde6e9336630c2cfb2a8
450255457 <450255457@qq.com>
2016-04-09 13:28:28 +0800

    04091328

--- wechat.py
+++ wechat.py
@@ -31,24 +31,18 @@ class MainHandler(tornado.web.RequestHandler):
     def post(self): 
         body = self.request.body
         data = ET.fromstring(body)
+        print ('data:%s' % data)
         toUser = data.find('ToUserName').text
         fromUser = data.find('FromUserName').text
-        createTime = int(time.time())
+        createTime = str(int(time.time()))
         msgType = data.find('MsgType').text
         content = data.find('Content').text
         msgId= data.find("MsgId").text
-        textTpl = """<xml>
-            <ToUserName><![CDATA[%s]]></ToUserName>
-            <FromUserName><![CDATA[%s]]></FromUserName>
-            <CreateTime>%s</CreateTime>
-            <MsgType><![CDATA[%s]]></MsgType>
-            <Content><![CDATA[%s]]></Content>
-            <MsgId>%s</MsgId>
-            </xml>"""
-        out = textTpl % (fromUser, toUser, createTime, msgType, content, msgId)
-        self.write(out)
+        print ('createTime:%s' % createTime)
+        self.render("reply_text.html", toUser=toUser, fromUser=fromUser, createTime=createTime, content=content)
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/reply_text", MainHandler),
 ])
 
 if __name__ == "__main__":
