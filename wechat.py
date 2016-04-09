961d8c2a1da6217b380b89544e5c39dc9ed63ac7
450255457 <450255457@qq.com>
2016-04-09 13:57:09 +0800

    04091357

--- wechat.py
+++ wechat.py
@@ -6,7 +6,39 @@ import tornado.web
 import hashlib
 import xml.etree.ElementTree as ET
 import time
+import urllib2, json
 
+response_text = """<xml>
+            <ToUserName><![CDATA[%s]]></ToUserName>
+            <FromUserName><![CDATA[%s]]></FromUserName>
+            <CreateTime>%s</CreateTime>
+            <MsgType><![CDATA[%s]]></MsgType>
+            <Content><![CDATA[%s]]></Content>
+            <MsgId>%s</MsgId>
+            </xml>"""
+ 
+def translate(data):
+    qword = urllib2.quote(data)
+    baseurl = r'http://fanyi.youdao.com/openapi.do?keyfrom=linden&key=1102672932&type=data&doctype=json&version=1.1&q='
+    url = baseurl+qword
+    resp = urllib2.urlopen(url)
+    fanyi = json.loads(resp.read())
+    if fanyi['errorCode'] == 0:
+        if 'basic' in fanyi.keys():
+            trans = u'%s:\n%s\n%s\n网络释义：\n%s'%(fanyi['query'], ''.join(fanyi['translation']), ' '.join(fanyi['basic']['explains']), ''.join(fanyi['web'][0]['value']))
+            return trans
+        else:
+            trans =u'%s:\n基本翻译:%s\n'%(fanyi['query'], ''.join(fanyi['translation']))
+            return trans
+    elif fanyi['errorCode'] == 20:
+        return u'对不起，要翻译的文本过长'
+    elif fanyi['errorCode'] == 30:
+        return u'对不起，无法进行有效的翻译'
+    elif fanyi['errorCode'] == 40:
+        return u'对不起，不支持的语言类型'
+    else:
+        return u'对不起，您输入的单词%s无法翻译,请检查拼写'% data
+            
 def check_signature(signature, timestamp, nonce):
     # 微信公众平台里输入的token
     token="linden"
@@ -37,15 +69,13 @@ class MainHandler(tornado.web.RequestHandler):
         msgType = data.find('MsgType').text
         content = data.find('Content').text
         msgId= data.find("MsgId").text
-        textTpl = """<xml>
-            <ToUserName><![CDATA[%s]]></ToUserName>
-            <FromUserName><![CDATA[%s]]></FromUserName>
-            <CreateTime>%s</CreateTime>
-            <MsgType><![CDATA[%s]]></MsgType>
-            <Content><![CDATA[%s]]></Content>
-            <MsgId>%s</MsgId>
-            </xml>"""
-        out = textTpl % (fromUser, toUser, createTime, msgType, content, msgId)
+        if 'text' == msgType:
+            if 'help' == content.lower():
+                content = u'''1.输入"translate"为中英翻译工具
+                '''
+            # elif 'translate' == content:
+            content = translate(content)
+            out = response_text % (fromUser, toUser, createTime, msgType, content, msgId)
         self.write(out)
 application = tornado.web.Application([
     (r"/", MainHandler),
